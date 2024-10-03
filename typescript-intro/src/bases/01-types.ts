6bdb66cc08043631983d5cf570c30b1e83a425f1
Matías Cruz <matias18233@gmail.com>
2024-10-03 20:18:40 -0300

    Uso de templateString

--- typescript-intro/src/bases/01-types.ts
+++ typescript-intro/src/bases/01-types.ts
@@ -1,7 +1,23 @@
-export const name = 'Fernando';
+export let name = 'Fernando';
 
 export const age: number = 35;
 
 export const isValid: boolean = true;
 
 console.log(isValid);
+
+name = 'Melisa';
+
+export const templateString = `Esto es un string
+multilínea
+que puede tener
+" dobles,
+' simples
+inyectar símbolos \$
+inyectar variables ${ name }
+expresiones ${ 1 + 1 }
+números ${ age }
+boolean ${ isValid }
+`;
+
+console.log(templateString);
