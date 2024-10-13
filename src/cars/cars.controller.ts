9fd3568f47d58199a57a233b31457d6fa7f9abe9
Matías Cruz <matias18233@gmail.com>
2024-10-13 17:55:43 -0300

    Creado método Get

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+    @Get()
+    getAllCars() {
+        return ['Toyota', 'Honda', 'Jeep'];
+    }
 }
