057cba277791e9134f44aea4da119ec78ad05f80
Matías Cruz <matias18233@gmail.com>
2024-10-13 17:55:25 -0300

    Se crea primer módulo y controlador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CarsModule } from './cars/cars.module';
 
 // El @ lo convierte en el módulo principal
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [],
   providers: [],
   exports: [],
---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+}
