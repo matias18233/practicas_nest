ec0ced5ba06303e9bd773f04b46df30734f4bbb4
Matías Cruz <matias18233@gmail.com>
2024-10-13 17:20:17 -0300

    Cambios iniciales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000); // Puerto donde se va a ejecutar la aplicación
 }
-bootstrap();
+main();
