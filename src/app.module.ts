847625b62412af9054a17fab54fa8f03564c0edb
Matías Cruz <matias18233@gmail.com>
2024-10-14 07:16:37 -0300

    Instalación de paquetes relacionados a postgres y configuración de base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // El @ lo convierte en el módulo principal
 @Module({
-  imports: [ CarsModule ],
+  imports: [
+    ConfigModule.forRoot(),
+    CarsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
   exports: [],
