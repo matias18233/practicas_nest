faa64f7a0065f4ace6969ba001903ce999cfac7c
Matías Cruz <matias18233@gmail.com>
2024-10-15 08:54:08 -0300

    Ajustes para realizar la pegada a la API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(3000); // Puerto donde se va a ejecutar la aplicación
 }
 main();
