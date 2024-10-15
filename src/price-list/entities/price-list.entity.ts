c2405de9fc665def47fc7a8d20881ca8af04d1ed
Matías Cruz <matias18233@gmail.com>
2024-10-15 10:48:29 -0300

    Se crea la entidad para que impacte en BD

--- src/price-list/entities/price-list.entity.ts
+++ src/price-list/entities/price-list.entity.ts
@@ -1,1 +1,30 @@
-export class PriceList {}
+import { Column, Entity, PrimaryGeneratedColumn, Timestamp } from "typeorm";
+
+@Entity()
+export class PriceList {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column('int')
+    companyId: number;
+
+    @Column('text')
+    description: string;
+
+    @Column('int')
+    defaultCurrencyId: number;
+
+    @Column('boolean')
+    isDisabled: boolean;
+
+    @Column('timestamp')
+    createdAt: Timestamp;
+
+    @Column('timestamp')
+    updatedAt: Timestamp;
+
+    @Column('timestamp')
+    deletedAt: Timestamp;
+
+}
--- src/price-list/price-list.module.ts
+++ src/price-list/price-list.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PriceListService } from './price-list.service';
 import { PriceListController } from './price-list.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PriceList } from './entities/price-list.entity';
 
 @Module({
   controllers: [PriceListController],
   providers: [PriceListService],
+  imports: [
+    TypeOrmModule.forFeature([ PriceList ])
+  ]
 })
 export class PriceListModule {}
