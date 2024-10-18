8481673aabfff59f25fcface5d53b579fd322839
Matías Cruz <matias18233@gmail.com>
2024-10-18 12:38:27 -0300

    Creado el método de creación de listas de precios

--- src/price-list/dto/create-price-list.dto.ts
+++ src/price-list/dto/create-price-list.dto.ts
@@ -1,1 +1,32 @@
-export class CreatePriceListDto {}
+import { Timestamp } from "typeorm";
+import { IsBoolean, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreatePriceListDto {
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    id: number;
+
+    @IsNumber()
+    @IsPositive()
+    companyId: number;
+
+    @IsString()
+    @MinLength(3)
+    description: string;
+
+    @IsNumber()
+    defaultCurrencyId: number;
+
+    @IsBoolean()
+    isDisabled: boolean;
+
+    @IsOptional()
+    createdAt?: Timestamp;
+
+    @IsOptional()
+    updatedAt?: Timestamp;
+
+    @IsOptional()
+    deletedAt?: Timestamp;
+}
--- src/price-list/price-list.service.ts
+++ src/price-list/price-list.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreatePriceListDto } from './dto/create-price-list.dto';
 import { UpdatePriceListDto } from './dto/update-price-list.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PriceList } from './entities/price-list.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PriceListService {
-  create(createPriceListDto: CreatePriceListDto) {
-    return 'This action adds a new priceList';
+
+  private readonly logger = new Logger('PriceListService');
+
+  constructor(
+    @InjectRepository(PriceList)
+    private readonly priceListRepository: Repository<PriceList>,
+  ) { }
+
+  async create(createPriceListDto: CreatePriceListDto) {
+    try {
+      const priceList = this.priceListRepository.create(createPriceListDto);
+      await this.priceListRepository.save(priceList);
+
+      return priceList;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
   }
 
   findAll() {
@@ -23,4 +41,13 @@ export class PriceListService {
   remove(id: number) {
     return `This action removes a #${id} priceList`;
   }
+  
+  private handleExceptions( error: any ) {
+    if ( error === '22505' ) {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs');
+  }
+
 }
